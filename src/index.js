c72f7ce5a089df87c445e169304139804d8dc188
Igor Ming <igor.ming@gmail.com>
2021-04-13 18:12:58 -0300

    using arrow function instead

--- src/index.js
+++ src/index.js
@@ -8,13 +8,11 @@ class App extends React.Component {
     this.state = {
       counter: 0,
     };
-
-    this.add = this.add.bind(this);
   }
 
-  add() {
+  add = () => {
     this.setState({ counter: this.state.counter + 1 });
-  }
+  };
 
   render() {
     return (
