24155a6d7f6e62bad1b038fb51e9a677bbd62438
Igor Ming <igor.ming@gmail.com>
2021-04-13 18:11:43 -0300

    configure react and use a class component

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      counter: 0,
+    };
+
+    this.add = this.add.bind(this);
+  }
+
+  add() {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>testando o counter: {this.state.counter}</h1>
+        <button onClick={this.add}>Add</button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
